6c5854861e94bc736a9ddaba63bbbed38b169e22
Ritika <sachdevaritika99@gmail.com>
2022-10-13 13:48:50 +0530

    first commit

---
+++ Jarvis.py
@@ -0,0 +1,125 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+import pyjokes #pip install pyjokes
+#comment
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Lexi. Please tell me how may I help you")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        r.energy_threshold = 100
+        #r.adjust_for_ambient_noise(source, duration= 1)
+        audio = r.listen(source) #,timeout= 5,phrase_time_limit= 8)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('yourmail@gmail.com', 'type-your-password')
+    server.sendmail('yourmail@gmail.com',to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stack overflow' in query:
+            webbrowser.open("https://stackoverflow.com/")   
+
+
+        elif 'play music' in query:
+            music_dir = 'C:\\Users\\user\\Music'
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M %p")    
+            speak(f"The time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\user\\AppData\\Local\\Programs\\Microsoft VS Code"
+            os.startfile(codePath)
+
+        elif 'email to Ritika' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "ritikasachdeva06@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("I am sorry my friend. I am not able to send this email")    
+        elif 'joke' in query:
+            speak(pyjokes.get_joke())
+
+        elif 'How are you' in query:
+            speak("I am fine. What about you?")
+        
+        elif "repeat after me" in query:
+            query = query.replace("repeat after me","")
+            speak(query)
+
+        elif 'bye' or 'stop' in query:
+            exit()
+        
